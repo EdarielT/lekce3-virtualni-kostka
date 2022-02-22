562ddde9da54a10920295e487b9d96e5eab7dd0a
Anastasia Buben <bubenastya@seznam.cz>
2022-02-22 20:39:30 +0100

    Rundom numbers, changing of the png and the text.

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
 // tady je místo pro náš program
+
+let play = () => {
+    let result = Math.floor(Math.random()*6+1);
+    if (result != 6) {
+        document.getElementById('zprava').innerHTML = `Hodila jsi ${result}. Zkus to znovu`;
+    } else {
+        document.getElementById('zprava').innerHTML = "Sestka!!! Vyhrala jsi!";
+    }
+    let newPng = `obrazky/${result}.png`;
+    document.getElementById('kostka').src = newPng; 
+
+}
+
+document.getElementById('zprava')
